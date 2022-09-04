94099b62e1d77bc635fa37290167121044a0ab81
pphetra <pphetra@gmail.com>
2022-09-04 14:22:43 +0700

    move to sidebar

--- components/layout/BasicLayout.jsx
+++ components/layout/BasicLayout.jsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
+
 export default function BasicLayout(props) {
   return (
     <div className="flex flex-row">
-      <div className="h-screen w-32 bg-yellow-50">
-        <div>sidebar</div>
+      <div className="h-screen w-32 bg-yellow-50 p-4">
+        <div>
+          <ul className="space-y-4">
+            <li>
+              <Link href="page1">Page1</Link>
+            </li>
+            <li>
+              <Link href="page2">Page2</Link>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="w-full">
         <div className="h-16 bg-blue-100">topbar</div>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,5 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="px-4 bg-blue-200">
-      <ul className="space-y-4">
-        <li>
-          <Link href="page1">Page1</Link>
-        </li>
-        <li>
-          <Link href="page2">Page2</Link>
-        </li>
-      </ul>
-    </div>
-  );
+  return <div className="px-4 bg-blue-200"></div>;
 }
