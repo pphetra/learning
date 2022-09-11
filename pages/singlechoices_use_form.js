26f2d7963078dd68fd88b0dfbd449f52c35c83e3
pphetra <pphetra@gmail.com>
2022-09-11 18:26:47 +0700

    refactor to component

--- pages/singlechoices_use_form.js
+++ pages/singlechoices_use_form.js
@@ -1,6 +1,60 @@
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 
+function Question(props) {
+  const {
+    label,
+    name,
+    options,
+    register,
+    watch,
+    infoName,
+    setValue,
+    infoChoice,
+  } = props;
+  const value = watch(name);
+  useEffect(() => {
+    if (value != infoChoice) {
+      setValue(infoName, "");
+    }
+  }, [infoChoice, setValue, value, infoName]);
+
+  return (
+    <div className="p-8">
+      <h3>
+        {label} ({name})
+      </h3>
+      <div>
+        {options.map((option) => (
+          <div key={option.label} className="flex gap-2 items-center p-2">
+            <input
+              type="radio"
+              value={option.value}
+              id={`${name}-${option.label}`}
+              {...register(name)}
+            />
+            <label htmlFor={`${name}-${option.label}`}>
+              {option.label} ({name})
+            </label>
+          </div>
+        ))}
+        {value === infoChoice && (
+          <div>
+            <label htmlFor="info">More info</label>
+            <input
+              id="info"
+              type="text"
+              name="info"
+              className="border"
+              {...register(infoName)}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
 export default function SingleChoiceUseForm() {
   const {
     register,
@@ -12,67 +66,58 @@ export default function SingleChoiceUseForm() {
     defaultValues: {
       choice: "1",
       info: "",
+      test: "1",
+      infoTest: "",
+      another: "2",
+      infoAnother: "",
     },
   });
-  const choice = watch("choice");
-
-  useEffect(() => {
-    if (choice != "3") {
-      setValue("info", "");
-    }
-  }, [choice, setValue]);
 
   const onSubmit = (data) => console.log(data);
+
+  const options = [
+    { label: "Choice 1", value: "1" },
+    { label: "Choice 3", value: "3" },
+    { label: "Choice 2", value: "2" },
+  ];
 
   return (
     <div className="bg-gray-50 h-screen flex">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="m-8 p-8 bg-white rounded w-full">
           <h2>Singlechoices</h2>
-          <div className="p-8">
-            <h3>Age group</h3>
-            <div>
-              <div className="flex gap-2 items-center p-2">
-                <input
-                  type="radio"
-                  id="choice1"
-                  value="1"
-                  {...register("choice")}
-                />
-                <label htmlFor="choice1">Choice 1</label>
-              </div>
-              <div className="flex gap-2 items-center p-2">
-                <input
-                  type="radio"
-                  id="choice2"
-                  value="2"
-                  {...register("choice")}
-                />
-                <label htmlFor="choice2">Choice 2</label>
-              </div>
-              <div className="flex gap-2 items-center p-2">
-                <input
-                  type="radio"
-                  id="choice3"
-                  value="3"
-                  {...register("choice")}
-                />
-                <label htmlFor="choice3">Choice 3</label>
-              </div>
-              {choice === "3" && (
-                <div>
-                  <label htmlFor="info">More info</label>
-                  <input
-                    id="info"
-                    type="text"
-                    name="info"
-                    className="border"
-                    {...register("info")}
-                  />
-                </div>
-              )}
-            </div>
-          </div>
+          <Question
+            label="Age Group"
+            name="choice"
+            infoName="info"
+            infoChoice="3"
+            options={options}
+            register={register}
+            setValue={setValue}
+            watch={watch}
+          />
+
+          <Question
+            label="test"
+            name="test"
+            infoName="infoTest"
+            infoChoice="3"
+            options={options}
+            register={register}
+            setValue={setValue}
+            watch={watch}
+          />
+
+          <Question
+            label="another"
+            name="another"
+            infoName="infoAnother"
+            infoChoice="3"
+            options={options}
+            register={register}
+            setValue={setValue}
+            watch={watch}
+          />
 
           <div>
             <button type="submit">Submit</button>
